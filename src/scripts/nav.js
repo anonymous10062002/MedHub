e430b4a202188b00bc805d160b9c4acc7de8b039
Rahul Sharma <40212890+257277@users.noreply.github.com>
2022-12-15 12:06:56 +0530

    Nav bar and footer adding by Rahul

---
+++ src/scripts/nav.js
@@ -0,0 +1,90 @@
+let opennav = () => {
+
+    if (document.querySelector(".dropDown").style.height == false || document.querySelector(".dropDown").style.height == "0px") {
+        document.querySelector(".dropDown").style.height = "200px";
+        document.querySelector(".menu").style.color = "blue";
+    }
+    else {
+        document.querySelector(".dropDown").style.height = "0px";
+        document.querySelector(".menu").style.color = "black";
+    }
+
+}
+let opennav1 = () => {
+
+    if (document.querySelector(".vitamin").style.height == false || document.querySelector(".vitamin").style.height == "0px") {
+        document.querySelector(".vitamin").style.height = "350px";
+        document.querySelector(".vitamin1").style.color = "blue";
+    }
+    else {
+        document.querySelector(".vitamin").style.height = "0px";
+        document.querySelector(".vitamin1").style.color = "black";
+    }
+}
+let opennav2 = () => {
+
+    if (document.querySelector(".diabetes").style.height == false || document.querySelector(".diabetes").style.height == "0px") {
+        document.querySelector(".diabetes").style.height = "350px";
+        document.querySelector(".diabetes1").style.color = "blue";
+    }
+    else {
+        document.querySelector(".diabetes").style.height = "0px";
+        document.querySelector(".diabetes1").style.color = "black";
+    }
+}
+let opennav3 = () => {
+
+    if (document.querySelector(".Health").style.height == false || document.querySelector(".Health").style.height == "0px") {
+        document.querySelector(".Health").style.height = "350px";
+        document.querySelector(".Health1").style.color = "blue";
+    }
+    else {
+        document.querySelector(".Health").style.height = "0px";
+        document.querySelector(".Health1").style.color = "black";
+    }
+}
+let opennav4 = () => {
+
+    if (document.querySelector(".Personal").style.height == false || document.querySelector(".Personal").style.height == "0px") {
+        document.querySelector(".Personal").style.height = "350px";
+        document.querySelector(".Personal1").style.color = "blue";
+    }
+    else {
+        document.querySelector(".Personal").style.height = "0px";
+        document.querySelector(".Personal1").style.color = "black";
+    }
+}
+let opennav5 = () => {
+
+    if (document.querySelector(".HealthC ").style.height == false || document.querySelector(".HealthC ").style.height == "0px") {
+        document.querySelector(".HealthC ").style.height = "350px";
+        document.querySelector(".HealthC1").style.color = "blue";
+    }
+    else {
+        document.querySelector(".HealthC ").style.height = "0px";
+        document.querySelector(".HealthC1").style.color = "black";
+    }
+}
+let opennav6 = () => {
+
+    if (document.querySelector(".Ayurveda ").style.height == false || document.querySelector(".Ayurveda ").style.height == "0px") {
+        document.querySelector(".Ayurveda").style.height = "350px";
+        document.querySelector(".Ayurveda1").style.color = "blue";
+
+    }
+    else {
+        document.querySelector(".Ayurveda").style.height = "0px";
+        document.querySelector(".Ayurveda1").style.color = "black";
+    }
+}
+let opennav7 = () => {
+
+    if (document.querySelector(".homopathy").style.height == false || document.querySelector(".homopathy").style.height == "0px") {
+        document.querySelector(".homopathy").style.height = "350px";
+        document.querySelector(".homopathy1").style.color = "blue";
+    }
+    else {
+        document.querySelector(".homopathy").style.height = "0px";
+        document.querySelector(".homopathy1").style.color = "black";
+    }
+}
