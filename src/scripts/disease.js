526f603fffc6d4589022bd6523076b8f2608e8f6
Rahul Sharma <40212890+257277@users.noreply.github.com>
2022-12-16 22:14:04 +0530

    adding disease page

---
+++ src/scripts/disease.js
@@ -0,0 +1,57 @@
+let disease = document.querySelector("#disease");
+let data = [];
+let totalPages = Math.ceil(75 / 6) - 1;
+let baseurl = "https://639ada81d51415019741833a.mockapi.io/disease";
+let defaulturl = "https://639ada81d51415019741833a.mockapi.io/disease?page=1&limit=6";
+async function diseasefetch(url) {
+    try {
+        let req = await fetch(url);
+        let result = await req.json();
+        data = result;
+        let arr = data.map((item) => {
+            return `<div class="disease">
+            <div class="intro">
+         <img src="${item.image}" class="diseaseImage"></img>
+         <h1 class="diseaseName">${item.name}</h1>
+         </div>
+         <div class="diseaseDescription">
+         <h3>Overview</h3>
+         <p>${item.description}<p>
+         </div>
+        </div>`
+        })
+        disease.innerHTML = arr.join(" ");
+    }
+    catch (error) {
+        alert(error);
+    }
+}
+
+diseasefetch(defaulturl);
+
+function displayAllButtons(totalPages) {
+    document.querySelector("#pagination").innerHTML = `
+    ${createAllButton(totalPages).join(" ")}
+    `
+}
+displayAllButtons(totalPages);
+let arr = document.querySelectorAll(".pageButton");
+for (let page of arr) {
+    page.addEventListener("click", (event) => {
+        let x = event.target.id;
+        let url = baseurl + `?page=${x}&limit=6`;
+        diseasefetch(url);
+    })
+}
+
+function getButton(txt) {
+    return `<button class="pageButton" id="${txt}">${txt}</button>`
+}
+function createAllButton(totalPages) {
+    let buttonArray = [];
+    for (let i = 1; i <= totalPages; i++) {
+        buttonArray.push(getButton(i));
+    }
+    return buttonArray;
+}
+
